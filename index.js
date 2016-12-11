6a135a3f7dd3c1972d69e26264d01944164c4808
Otis Chou <altispec@gmail.com>
2016-12-11 18:42:38 +0800

    turn it into cli tool

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-#!/usr/bin/env node
-'use strict'
-const meow = require('meow')
-const api = require('./api')
+const loadJsonFile = require('load-json-file')
+const paths = require('path')
+const glob = require('glob')
+const isEmpty = require('lodash/isEmpty')
 
-const cli = meow(`
-  Usage
-    $ config-upload
+const DEFAULT_CONFIG_PATH =  '.uploadrc'
 
-  Options
-  -n, --no-fail-fast the process will not exit with failure until finish all tasks
+function api (cli) {
+  const configs = loadJsonFile.sync(cli.flags.configs || DEFAULT_CONFIG_PATH)
 
-  -c, --configs specify configuration file path. default is .uploadrc
-
-  --variables variables for configuration
-
-  Examples
-    $ config-upload '{"revision": "v1.2.3"}'
-`, {
-  alias: {
-    n: 'no-fail-fast',
-    c: 'configs'
+  if (!configs.sources || configs.sources.length === 0) {
+    throw new Error('Should contains sources configuration')
   }
-})
 
-api(cli)
+  configs.sources.forEach(function (source) {
+    _uploadSource(source, configs)
+  })
+}
+
+function _uploadSource(source, configs) {
+  if (!source.include) {
+    throw new Error('"include" should not be empty.')
+  }
+
+  if (!source.dist) {
+    throw new Error('"include" should not be empty.')
+  }
+
+  const distConfig = configs.dists[source.dist]
+  if (!distConfig || isEmpty(distConfig)) {
+    throw new Error(`Distination "${source.dist}" is not found or empty.`)
+  }
+
+  const includeFiles = glob.sync(source.include, { nodir: true, ignore: source.exclude })
+  const uploader = _uploader(distConfig.type || source.dist, distConfig)
+
+  includeFiles.forEach(function(file) {
+    uploader.upload(file, source.folder)
+  })
+}
+
+function _uploader(type, options) {
+  switch (type) {
+    case 's3':
+      const Uploader = require('./uploaders/s3')
+      return new Uploader(options)
+    default:
+      throw new Error(`No correspond uploader "${type}"`)
+  }
+}
+
+module.exports = api
