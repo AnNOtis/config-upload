a4e76feeb260dbd1a7b822ab452fd1ca71977d83
Otis Chou <altispec@gmail.com>
2016-12-13 02:14:36 +0800

    update readme

--- bin/cli.js
+++ bin/cli.js
@@ -8,9 +8,9 @@ const cli = meow(`
     $ config-upload [options]
 
   Options
-    --no-fail-fast Upload all files no matter what. Default will exiting on first failure.
-    --config Config file path. Default is ".config-upload.json".
-    --context Additional context provided to config.
+    --no-fail-fast   Upload all files no matter what. Default will exiting on first failure.
+    --config         Config file path. Default is ".config-upload.json".
+    --context        Additional context provided to config.
 
   Examples
     $ config-upload
